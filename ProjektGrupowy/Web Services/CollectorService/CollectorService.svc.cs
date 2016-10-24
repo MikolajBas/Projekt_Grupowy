f64fcba3b2c858e0e60a151bf0b0a8ad678ed675
sosna-git <sosna321@gmail.com>
2016-10-24 20:32:54 +0200

    Implement CollectorService methods

--- ProjektGrupowy/Web Services/CollectorService/CollectorService.svc.cs
+++ ProjektGrupowy/Web Services/CollectorService/CollectorService.svc.cs
@@ -7,11 +7,8 @@ using NHibernate.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
-using System.ServiceModel.Web;
-using System.Text;
 
 namespace CollectorService
 {
@@ -25,12 +22,24 @@ namespace CollectorService
         //     and include the following line in the operation body:
         //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
         [OperationContract]
-        public void Collect(string jsonData, string userGuid)
+        public void Collect(string jsonData, string jsonDataProperties, string userGuid)
         {
             var serializer = new DataSerializer();
             var data = serializer.Deserialize<Data>(jsonData);
-            
-            return;
+            var dataProperties = serializer.Deserialize<List<DataProperty>>(jsonDataProperties);
+
+            using (var session = Connector.OpenSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                session.Update(data);
+
+                foreach (var dataProperty in dataProperties)
+                {
+                    session.Update(dataProperty);
+                }
+
+                transaction.Commit();
+            }
         }
 
         [OperationContract]
@@ -49,7 +58,9 @@ namespace CollectorService
                 {
                     if (configurator.CheckEvent(session, config, ip))
                     {
+                        var data = configurator.GetSmartResult(session, config, ip);
 
+                        return configurator.ParseTemplate(session, config, data);
                     }
                 }
 
