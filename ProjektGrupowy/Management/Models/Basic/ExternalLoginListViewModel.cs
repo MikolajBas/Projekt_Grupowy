d57d0b4c4cb775824d434e08864334319bed86a3
sosna-git <sosna321@gmail.com>
2016-10-29 23:53:38 +0200

    Refactor of basic models in mvc app

---
+++ ProjektGrupowy/Management/Models/Basic/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Management.Models
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ ProjektGrupowy/Management/Models/Basic/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Management.Models
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
