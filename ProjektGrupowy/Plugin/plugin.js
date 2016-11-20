b6b001080b0a669e81a691a139682c5136bfacab
MikolajBas <mikolaj1bas@gmail.com>
2016-11-20 02:05:33 +0100

    Add browser and device info to plugin

--- ProjektGrupowy/Plugin/plugin.js
+++ ProjektGrupowy/Plugin/plugin.js
@@ -1,6 +1,19 @@
 
 $( document ).ready(function() {
-    $.get( "http://ip-api.com/json?callback=?", function(data) {
-        alert(JSON.stringify(data));
+    //browser os and device info
+    var client = new ClientJS();
+    var data = {};
+    data.timezone = client.getTimeZone();
+    data.language = client.getLanguage();
+    data.resolution = client.getCurrentResolution();
+    data.mobile = client.isMobile();
+    data.device = client.getDevice();
+    data.os = client.getOS();
+    data.browser = client.getBrowser();
+    alert(JSON.stringify(data));
+
+    //localisation info
+    $.get( "http://ip-api.com/json?callback=?", function(json) {
+        alert(JSON.stringify(json));
     });
 });
