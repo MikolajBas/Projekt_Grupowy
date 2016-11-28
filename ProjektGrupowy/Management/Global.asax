3cb008d60492464fa3e130a5fc9d35379d227e60
sosna-git <sosna321@gmail.com>
2016-11-28 21:37:11 +0100

    Remove Management, its obsolute

--- ProjektGrupowy/Management/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Management.MvcApplication" Language="C#" %>
---
+++ ProjektGrupowy/Panel/Models/Basic/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Panel.Models
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
