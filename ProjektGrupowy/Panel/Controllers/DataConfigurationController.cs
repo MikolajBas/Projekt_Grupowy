19065939627eb87fba631b49f245aef8ad6ca1e3
sosna-git <sosna321@gmail.com>
2016-12-03 18:37:41 +0100

    Add basic subpages

---
+++ ProjektGrupowy/Panel/Controllers/DataConfigurationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Panel.Controllers
+{
+    public class DataConfigurationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
