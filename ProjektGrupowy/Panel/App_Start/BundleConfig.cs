0582eaef584041e0c4c598fb1ae3a80d0f9a2977
Mikolaj Bas <Mikolaj Bas>
2016-12-07 20:52:45 +0100

    Add chart.js library

--- ProjektGrupowy/Panel/App_Start/BundleConfig.cs
+++ ProjektGrupowy/Panel/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace Panel
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+                      "~/Scripts/Chart.js"));
         }
     }
 }
