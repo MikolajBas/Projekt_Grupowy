9ce0664b055956a9f081915dec98ccf89e0351d3
MikolajBas <mikolaj1bas@gmail.com>
2016-12-07 18:56:45 +0100

    Send collected data to server and get advert template + some small refactoring

--- ProjektGrupowy/Plugin/plugin.js
+++ ProjektGrupowy/Plugin/plugin.js
@@ -1,19 +1,35 @@
 
-$( document ).ready(function() {
-    //browser os and device info
     var client = new ClientJS();
-    var data = {};
-    data.timezone = client.getTimeZone();
-    data.language = client.getLanguage();
-    data.resolution = client.getCurrentResolution();
-    data.mobile = client.isMobile();
-    data.device = client.getDevice();
-    data.os = client.getOS();
-    data.browser = client.getBrowser();
-    alert(JSON.stringify(data));
+    var data = {}, advert = {};
 
-    //localisation info
-    $.get( "http://ip-api.com/json?callback=?", function(json) {
-        alert(JSON.stringify(json));
+    if(client) {
+        data.timezone = client.getTimeZone();
+        data.language = client.getLanguage();
+        data.resolution = client.getCurrentResolution();
+        data.mobile = client.isMobile();
+        data.device = client.getDevice();
+        data.os = client.getOS();
+        data.browser = client.getBrowser();
+    }
+
+    $.get( "http://ip-api.com/json?callback=", function(response) {
+        if(response) {
+            data.ip = response.query;
+            data.country = response.country;
+            data.region = response.region;
+            data.city = response.city;
+        }
+        //send collected information about user
+        $.post("http://localhost:3000/Collect",
+        {
+            jsonData: JSON.stringify(data),
+            jsonDataProperties: {},
+            userGuid: "838aa83a-b95e-11e6-a4a6-cec0c932ce01"
+        });
     });
-});
+
+
+    //get advert template from server
+    $.get( "http://localhost:3000/GetResponse", function(response) {
+        advert = response.advert;
+    });s
