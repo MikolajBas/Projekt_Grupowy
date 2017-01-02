69ee5870d5c30cf071934fc1709d66c3a31dfe25
sosna-git <sosna321@gmail.com>
2017-01-02 23:16:05 +0100

    Add Event Configuration view improvements

--- ProjektGrupowy/Panel/Controllers/EventConfigurationController.cs
+++ ProjektGrupowy/Panel/Controllers/EventConfigurationController.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Panel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace Panel.Controllers
@@ -11,6 +11,41 @@ namespace Panel.Controllers
     public class EventConfigurationController : Controller
     {
         public ActionResult Index()
+        {
+            var model = new EventConfigurationViewModel()
+            {
+                Configurations = new List<EventConfigRow>
+                {
+                    new EventConfigRow
+                    {
+                        Id = 1,
+                        Rule = "Rule rule rule",
+                        Period = 3
+                    },
+                    new EventConfigRow
+                    {
+                        Id = 2,
+                        Rule = "Rule rule rule2",
+                        Period = 15
+                    },
+                    new EventConfigRow
+                    {
+                        Id = 3,
+                        Rule = "Rule rule rule3",
+                        Period = 31
+                    }
+                }
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int id)
         {
             return View();
         }
---
+++ ProjektGrupowy/Panel/Models/EventConfigurationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Panel.Models
+{
+    public class EventConfigurationViewModel
+    {
+        public List<EventConfigRow> Configurations { get; set; }
+    }
+
+    public class EventConfigRow
+    {
+        public int Id { get; set; }
+        
+        public string Rule { get; set; }
+
+        public int Period { get; set; }
+    }
+}
