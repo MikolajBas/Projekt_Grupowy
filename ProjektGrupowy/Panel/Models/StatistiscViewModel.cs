01b67346acc267feeef39df7d723fa901c31fad8
Mikolaj Bas <Mikolaj Bas>
2017-01-06 21:52:14 +0100

    Add missing model file

---
+++ ProjektGrupowy/Panel/Models/StatistiscViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Panel.Models
+{ 
+    public class StatistiscViewModel
+    {
+
+        public List<AttributeType> Attributes { get; set; }
+
+        public List<AttributeType> ChartTypes { get; set; }
+
+        public int CurrentChartType;
+
+        public int CurrentAttribute;
+    }
+
+    public class AttributeType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public AttributeType(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
