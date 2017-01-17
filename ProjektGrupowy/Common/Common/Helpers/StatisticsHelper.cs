5bb97d8bdae94dde55ff4ca2c3c7afd5a402be1d
mikol <mikol@DESKTOP-LOCG8VS>
2017-01-17 21:50:25 +0100

    Statistics backend: add working method for retrieving data by parameter from db

---
+++ ProjektGrupowy/Common/Common/Helpers/StatisticsHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database.Models;
+using Database;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Transform;
+
+namespace Common.Helpers
+{
+    public class SiteCount
+    {
+        public string Name;
+        public int Count;
+    }
+
+    public static class StatisticsHelper
+    {
+        public static IList<SiteCount> GetSitesStatistics(string parameterName, int userId, DateTime startDate, DateTime endDate)
+        {
+            using (var session = Connector.OpenSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                var records = session.CreateCriteria<Database.Models.Data>()
+                    .SetProjection(Projections.ProjectionList()
+                        .Add(Projections.Alias(Projections.GroupProperty(parameterName), "Name"))
+                        .Add(Projections.Alias(Projections.Count(Projections.Id()), "Count")))
+                    .SetResultTransformer(Transformers.AliasToBean(typeof(SiteCount)))
+                    .Add(Expression.Between("Date", startDate, endDate))
+                    .Add(Expression.Eq("UserId", userId))
+                    .List<SiteCount>();
+
+                transaction.Commit();
+
+                return records;
+            }
+        }
+    }
+}
